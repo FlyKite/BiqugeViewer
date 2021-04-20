e414956d381f549d3a63eacce12f4ef71aeec260
FlyKite <DogeFlyKite@gmail.com>
2021-04-20 19:48:48 +0800

    Initial commit

---
+++ BiqugeViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BiqugeViewer
+//
+//  Created by FlyKite on 2021/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
